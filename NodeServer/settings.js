125d609f041f0c00be172c23966844f9a1df674f
morepen <493891498@qq.com>
2020-05-19 17:36:22 +0800

    Update settings.js

--- NodeServer/settings.js
+++ NodeServer/settings.js
@@ -4,7 +4,7 @@
         serverPort: 3000,
         mysql: {
             database: 'hbtbwechat',
-            host: '192.168.1.64',
+            host: '127.0.0.1',
             user: 'assist',
             password: 'ipcamera',
             port: 3306,
@@ -12,13 +12,13 @@
             multipleStatements: true
         },
         sign_url:'http://127.0.0.1:8080/Token/servlet/GetEncrypt',
-        test_aesKey:'4020100ceshi0001',
-        online_aesKey:'4020100PAYURL417',
+        test_aesKey:'',
+        online_aesKey:'',
 
         //url_getToken:'http://cxhbwx.cpic.com.cn/tbwechat/wechat/image_upload',//test
         //proxy_url:null, //test
-        url_getToken:'http://10.187.231.220:807/wechat/image_upload',//product
-        proxy_url:'http://10.187.184.205:8081',//product
+        url_getToken:'http://127.0.0.1:807/wechat/image_upload',//product
+        proxy_url:'http://127.0.0.1:8081',//product
         
        
 
